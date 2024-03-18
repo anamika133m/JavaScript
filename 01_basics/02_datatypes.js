11328e5655dc7037b6e80f26b64bb242bdcd7155
ANAMIKA MUDOTIYA <91742884+anamika133m@users.noreply.github.com>
2024-03-18 11:57:53 +0000

    datatypes discussed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not a browser
+
+let name = "Anamika"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value assign nhi hui hai
+// symbol => unique
+
+// object
+
+console.log(typeof name) //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
